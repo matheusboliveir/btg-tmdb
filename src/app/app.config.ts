22ea52f19c06c55008ff2a0609c8a756661e5269
matheusboliveir <theus.braz18@gmail.com>
2025-08-24 01:51:30 -0300

    feature: listagem em cartaz, interceptor de autenticação e service tmdb

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAngularSvgIcon } from 'angular-svg-icon';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),provideAngularSvgIcon(), provideHttpClient(), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAngularSvgIcon(),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+  ],
 };
---
+++ src/app/core/components/tag/tag.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tag',
+  standalone: true,
+  imports: [],
+  templateUrl: './tag.component.html',
+  styleUrl: './tag.component.scss',
+})
+export class TagComponent {}
